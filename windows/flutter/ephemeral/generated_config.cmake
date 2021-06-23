057757abafd02a35f8118a8c62c72e290eb168d4
Enrico Nogueira <31519325+EnricoNog@users.noreply.github.com>
2021-06-22 22:10:20 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "F:\\development\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\Flutter\\payflow" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=F:\\development\\flutter"
+  "PROJECT_DIR=F:\\Flutter\\payflow"
+  "FLUTTER_ROOT=F:\\development\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\Flutter\\payflow\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\Flutter\\payflow"
+  "FLUTTER_TARGET=F:\\Flutter\\payflow\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\Flutter\\payflow\\.dart_tool\\package_config.json"
+)
